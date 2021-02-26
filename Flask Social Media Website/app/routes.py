b4b6a04f933dfdaa502a9c3a6d64e544f2eff740
Anish Yadav <yadavanish893@gmail.com>
2021-02-25 16:48:26 -0800

    web forms

--- Flask Social Media Website/app/routes.py
+++ Flask Social Media Website/app/routes.py
@@ -1,6 +1,31 @@
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginPage
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello World! This is the sample page."
+	user = {
+		'username': 'Anish Yadav'
+	}
+
+	posts = [
+		{
+		'author': {'username': 'binarybrooder'},
+		'post': 'What a beautiful thought!'
+		},
+		{
+		'author': {'username': 'floydbaba'},
+		'post': 'Every thought is beautiful!'
+		}
+	]
+
+	return render_template('index.html', title = 'Home', user = user, posts = posts)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+	form = LoginPage()
+	if form.validate_on_submit():
+		flash('Login requested by {}, remember_me = {}'.format(form.username.data, form.remember_me.data))
+		return redirect(url_for('index'))
+	return render_template('login.html', title = 'Sign-in page', form = form)
