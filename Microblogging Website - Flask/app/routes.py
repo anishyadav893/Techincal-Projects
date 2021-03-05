1ee63a42a8deb4b21e5989edd33b74df3dc4ea9e
Anish Yadav <yadavanish893@gmail.com>
2021-03-05 14:49:55 -0800

    template inheritance

--- Microblogging Website - Flask/app/routes.py
+++ Microblogging Website - Flask/app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello Mr. Anish Yadav!"
+	user = {'username': 'Mr. A'}
+	posts = [
+		{
+		'author': {'username': 'AY'},
+		'content': 'AY just want to go somewhere'
+		},
+		{
+		'author': {'username': "Yadav"},
+		'content': 'Give me some mental peace, fuck the sunshine, jk!'
+		}
+	]
+
+	return render_template('index.html', title = 'Home', user = user, posts = posts)
